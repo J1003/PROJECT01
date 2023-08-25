12a98c585e0d4eb3f612cff8c2b2aab50d79e79a
WIN-LGGT3OHDDJC\itwill <itwill@WIN-LGGT3OHDDJC>
2023-08-25 20:04:03 +0900

    test

---
+++ ttttt/src/ttttt/NumberInSquare.java
@@ -0,0 +1,39 @@
+package ttttt;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NumberInSquare {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Number in Square");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new DrawingPanel());
+            frame.setSize(300, 300);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class DrawingPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int squareSize = 50;
+        int numRows = 5;
+        int numCols = 8;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                int number = row * numCols + col + 1;
+                int x = col * squareSize;
+                int y = row * squareSize;
+                
+                g.drawRect(x, y, squareSize, squareSize);
+                g.drawString(String.valueOf(number), x + squareSize / 2, y + squareSize / 2);
+            }
+        }
+    }
+}
+
+
